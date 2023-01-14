9668f6b8c1d583286590d27248bcb05e139c3940
sales@merrows.co.uk <sales@merrows.co.uk>
2023-01-14 13:50:50 +0000

    edge

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css'
 
+export const config = {
+  runtime: 'edge',
+}
+
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+export const config = {
+  runtime: 'edge',
+}
+
+
 export default function Document() {
   return (
     <Html lang="en">
